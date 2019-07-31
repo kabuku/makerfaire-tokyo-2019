22450e090e4d3ba2d16da38b6e408f43fd4738d0
shouta.yoshikai <shouta.yoshikai@gmail.com>
2019-07-31 19:59:14 +0900

    fix controller code

--- robo_controller/con1.sh
+++ robo_controller/con1.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-python controller.py --id=0 --robot_name=nobunaga --broker=localhost
+python controller.py --id=0 --robot_name=nobunaga --broker=localhost --left_max=12 --left_min=4.0 --right_max=12 --right_min=4
